039118d9e156a473b440a07e0d57ca2352a559ab
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-12 03:40:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var rls = document.querySelectorAll('ul.ranked-list');
+  for(let i =0; i<rls.length; i++){
+    let num = parseInt(rls[i].innerHTML);
+    rls[i].innerHTML = (num + n).toString();
+  }
+}
+
+function deepestChild(){
+  let node = document.querySelector('#grand-node');
+  let chld = node.children[0];
+  
+  while(chld){
+    node = chld;
+    chld = chld.children[0];
+  }
+  
+  return node;
+}
+    
+
